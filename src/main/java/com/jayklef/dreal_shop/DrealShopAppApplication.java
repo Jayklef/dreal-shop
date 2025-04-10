8b03b301ba84e6b1351c7409144f5ca5a2dbb8b1
Jayklef <arhawhojerry@gmail.com>
2025-04-10 23:02:43 +0100

    Initial project setup

---
+++ src/main/java/com/jayklef/dreal_shop/DrealShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.dreal_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrealShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrealShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayklef/dreal_shop/DrealShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayklef.dreal_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrealShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
